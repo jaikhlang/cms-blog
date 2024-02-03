360bdad1338079dd3a542090116b0025949bad14
Jaikhlang Brahma <jaikhlang@gmail.com>
2024-02-03 13:59:40 +0530

    cleaning

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,4 +1,6 @@
 <div class="text-xl">
-  <img class="max-w-xs" src="{{ asset('jxn-logo.png') }}" alt="Employment News">
+  <img class="h-12" src="{{ asset('jxn-logo.png') }}" alt="Employment News">
   <!-- Filament <span class="text-primary-500 font-medium">Starter</span> -->
 </div>
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,14 @@ module.exports = {
       colors: {
         primary: colors.blue,
       },
+      height: {
+        'h-7': '1.75rem',
+        'h-8': '2rem',
+        'h-9': '2.25rem',
+        'h-10': '2.5rem',
+        'h-12': '3rem',
+        'h-14': '3.5rem',
+      },
     },
   },
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
