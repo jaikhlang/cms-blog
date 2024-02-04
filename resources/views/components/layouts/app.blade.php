0b925b340d690dc43781a88c81565ec8026dcfad
Jaikhlang Brahma <jaikhlang@gmail.com>
2024-02-04 22:37:16 +0530

    fix

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -22,16 +22,5 @@
 
     @livewireScriptConfig
     @stack('scripts')
-
-    <script>
-      // grab everything we need
-      const btn = document.querySelector("button.mobile-menu-button");
-      const menu = document.querySelector(".mobile-menu");
-
-      // add event listeners
-      btn.addEventListener("click", () => {
-        menu.classList.toggle("hidden");
-      });
-    </script>
   </body>
 </html>
